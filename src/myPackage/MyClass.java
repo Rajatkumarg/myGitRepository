f2ac96f97bdda0c56f29153206494e17b08cd371
ckhuntwal <khuntwalc15@gmail.com>
2023-08-18 13:15:43 +0530

    'second-commit'

--- src/myPackage/MyClass.java
+++ src/myPackage/MyClass.java
@@ -2,5 +2,9 @@
 package myPackage;
 
 public class MyClass {
-    
+    public static void main(String[] args) {
+        System.out.println("hello-1");
+        System.out.println("hello-2");
+        System.out.println("hello-3");
+    }
 }
